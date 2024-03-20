85aad2f7013b3f19218a49efa8c48f50019db0bb
wmsilva0000 <138164539+wmsilva0000@users.noreply.github.com>
2024-03-20 17:54:21 -0300

    Delete web.php

--- web.php
+++
@@ -1,9 +0,0 @@
-<?php
-//atividade1_a
-Route::get('/{nome}/{sobrenome?}', 'NomeCompleto@Mostrar');
-
-//atividade1_b
-Route::get('/operacoes/{tipo}/{valor1}/{valor2}', 'OperacoesController@Calculo');
-
-//atividade1_c
-Route::get('/dados/{id}', 'DadosPessoais@mostrarDados')->name('dados-pessoais.show');
